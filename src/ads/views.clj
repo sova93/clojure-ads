902a46519fa4c9fe3f67e5834b0a151286397628
sova93 <s_o_va@tut.by>
2016-12-18 20:14:46 +0300

    missed files

--- src/ads/views.clj
+++ src/ads/views.clj
@@ -2,7 +2,6 @@
   (:require [clojure.java.io]
             [ring.util.response :refer [response redirect]]
             [ads.models :as m]
-            [selmer.parser :refer [render-file]]
             [compojure.route :as route]
             [ads.helpers :as h]
             [ads.validations :as valid]
@@ -11,40 +10,37 @@
 
 (defn index [req]
   (log/log-message-to-service "index requested")
-  (render-file "index.html" {
-                             :cats    (m/get-cats)
-                             :ads     (m/get-ads)
-                             :sess    (:session req)
-                             :counter (h/register-new-request-to-counter req)})
+  (h/file-renderer req "index.html"
+                   {
+                    :cats (m/get-cats)
+                    :ads  (m/get-ads)
+                    })
   )
 
 (defn login [req]
   (log/log-message-to-service "login requested")
   (if (h/is-authenticated? req)
     (redirect "/")
-    (render-file "login.html" {
-                               :sess    (:session req)
-                               :counter (h/register-new-request-to-counter req)
-                               }))
+    (h/file-renderer req "login.html" {}))
   )
 (defn ads [req]
   (log/log-message-to-service "ads requested")
-  (render-file "ads.html" {
-                           :ads     (m/get-ads)
-                           :counter (h/register-new-request-to-counter req)
-                           }))
+  (h/file-renderer req "ads.html"
+                   {
+                    :ads (m/get-ads)
+                    }))
 
 (defn add-ads [req]
   (log/log-message-to-service "add ads requested")
-  (render-file "add-ads.html" {
-                               :cats    (m/get-cats)
-                               :counter (h/register-new-request-to-counter req)
-                               }))
+  (h/file-renderer req "add-ads.html"
+                   {
+                    :cats (m/get-cats)
+                    }))
 
 (defn post-add-ads [{{category_id :category_id title :title discription :description telephone :telephone} :params :as req}]
   (log/log-message-to-service "post-add-ads requested")
-  (m/insert-ad  category_id (get-in req [:session :identity :id] nil) title discription telephone )
-  (render-file "add-ads.html" {:req req :counter (h/register-new-request-to-counter req)}))
+  (m/insert-ad category_id (get-in req [:session :identity :id] nil) title discription telephone)
+  (h/file-renderer req "add-ads.html" {}))
 
 (defn post-login [{{login :login password :password} :params session :session :as req}]
   (log/log-message-to-service "post-login requested")
@@ -55,7 +51,11 @@
       (do
         (assoc (redirect "/") :session (assoc session :identity (select-keys user-from-db [:login :id]))))
       (do
-        (render-file "login.html" {:error_message "Something wrong with your credentials!" :q user-from-db :req req :counter counter}))
+        (h/file-renderer req "login.html"
+                         {
+                          :error_message "Something wrong with your credentials!"
+                          :q             user-from-db
+                          }))
       )
     )
   )
@@ -69,24 +69,23 @@
   (log/log-message-to-service "signup requested")
   (if (h/is-authenticated? req)
     (redirect "/")
-    (render-file "signup.html" {
-                                :sess               (:session req)
-                                :login_ok           true
-                                :passwd_ok          true
-                                :password_repeat_ok true
-                                :email_ok           true
-                                :email_repeat       true
-                                :sex_ok             true
-                                :name_ok            true
-                                :surname_ok         true
-                                :lastname_ok        true
-                                :country_ok         true
-                                :city_ok            true
-                                :phone_ok           true
-                                :add_phone_ok       true
-                                :signed_ok          true
-                                :counter            (h/register-new-request-to-counter req)
-                                }))
+    (h/file-renderer req "signup.html"
+                     {
+                      :login_ok           true
+                      :passwd_ok          true
+                      :password_repeat_ok true
+                      :email_ok           true
+                      :email_repeat       true
+                      :sex_ok             true
+                      :name_ok            true
+                      :surname_ok         true
+                      :lastname_ok        true
+                      :country_ok         true
+                      :city_ok            true
+                      :phone_ok           true
+                      :add_phone_ok       true
+                      :signed_ok          true
+                      }))
 
   )
 
@@ -123,55 +122,54 @@
         add_phone_ok (and (valid/check-len add_phone 13) (valid/check-phone? add_phone))
         signed_ok (valid/check-range signed 2 10)
 
-        counter (h/register-new-request-to-counter req)
         ]
     (if (and login_ok passwd_ok password_repeat_ok email_ok email_repeat sex_ok name_ok surname_ok lastname_ok country_ok city_ok phone_ok add_phone_ok signed_ok)
       (do
-        (m/insert-user {:login     login
-                        :password  password
-                        :email     email
-                        :sex       sex
-                        :name      name
-                        :surname   surname
-                        :lastname  lastname
-                        :country   country
-                        :city      city
-                        :phone     phone
-                        :add_phone add_phone
-                        })
+        (m/insert-user
+          {
+           :login     login
+           :password  password
+           :email     email
+           :sex       sex
+           :name      name
+           :surname   surname
+           :lastname  lastname
+           :country   country
+           :city      city
+           :phone     phone
+           :add_phone add_phone
+           })
         (assoc (redirect "/") :session (assoc session :identity (select-keys (h/login-user login password) [:login :id])))
         )
-      (render-file "signup.html" {:req                req
-                                  :login              login
-                                  :login_ok           login_ok
-                                  :passwd_ok          passwd_ok
-                                  :password_repeat_ok password_repeat_ok
-                                  :email              email
-                                  :email_ok           email_ok
-                                  :email_repeat       email_repeat
-                                  :sex                sex
-                                  :sex_ok             sex_ok
-                                  :name               name
-                                  :name_ok            name_ok
-                                  :surname            surname
-                                  :surname_ok         surname_ok
-                                  :lastname           lastname
-                                  :lastname_ok        lastname_ok
-                                  :country            country
-                                  :country_ok         country_ok
-                                  :city               city
-                                  :city_ok            city_ok
-                                  :phone              phone
-                                  :phone_ok           phone_ok
-                                  :add_phone          add_phone
-                                  :add_phone_ok       add_phone_ok
-                                  :signed_ok          signed_ok
-                                  :add_count          (count phone)
-                                  :add_count2         (valid/check-phone? phone)
-
-                                  :counter            counter
-
-                                  })))
+      (h/file-renderer req "signup.html"
+                       {
+                        :login              login
+                        :login_ok           login_ok
+                        :passwd_ok          passwd_ok
+                        :password_repeat_ok password_repeat_ok
+                        :email              email
+                        :email_ok           email_ok
+                        :email_repeat       email_repeat
+                        :sex                sex
+                        :sex_ok             sex_ok
+                        :name               name
+                        :name_ok            name_ok
+                        :surname            surname
+                        :surname_ok         surname_ok
+                        :lastname           lastname
+                        :lastname_ok        lastname_ok
+                        :country            country
+                        :country_ok         country_ok
+                        :city               city
+                        :city_ok            city_ok
+                        :phone              phone
+                        :phone_ok           phone_ok
+                        :add_phone          add_phone
+                        :add_phone_ok       add_phone_ok
+                        :signed_ok          signed_ok
+                        :add_count          (count phone)
+                        :add_count2         (valid/check-phone? phone)
+                        })))
 
   )
 
@@ -179,9 +177,13 @@
   (log/log-message-to-service "category requested")
   (let [cat (m/get-cat category-slug)]
     (if cat
-      (render-file "category.html" {:cat cat :ads (m/get-ads-by-cat category-slug) :sess (:session req) :counter (h/register-new-request-to-counter req)})
+      (h/file-renderer req "category.html"
+                       {
+                        :cat cat
+                        :ads (m/get-ads-by-cat category-slug)
+                        })
       (route/not-found "Not found"))))
 
 (defn ad [category-slug ad-id req]
   (log/log-message-to-service "ad requested")
-  (render-file "ad.html" {:sess (:session req) :counter (h/register-new-request-to-counter req)}))
+  (h/file-renderer req "ad.html" {}))
